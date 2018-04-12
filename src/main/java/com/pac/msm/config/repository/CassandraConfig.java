a397b7e861b0b6f015d2dd383b57051b6c553b65
Manjiri Namjoshi <mnamjoshi@paciolan.com>
2018-04-11 22:08:11 -0700

    adding ip translator

--- src/main/java/com/pac/msm/config/repository/CassandraConfig.java
+++ src/main/java/com/pac/msm/config/repository/CassandraConfig.java
@@ -1,5 +1,7 @@
 package com.pac.msm.config.repository;
 
+import java.net.InetSocketAddress;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +19,7 @@ import org.springframework.data.cassandra.mapping.CassandraMappingContext;
 import org.springframework.data.cassandra.mapping.SimpleUserTypeResolver;
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.policies.AddressTranslator;
 
 /**
  * Configuration for cassandra database.
@@ -34,6 +37,28 @@ public class CassandraConfig {
         cluster.setPort(Integer.parseInt(environment.getProperty("cassandra.port")));
         cluster.setUsername(environment.getProperty("cassandra.username"));
         cluster.setPassword(environment.getProperty("cassandra.password"));
+        final String template = "*.21.*.*:*.17.*.*,*.22.*.*:*.18.*.*";
+        cluster.setAddressTranslator(new AddressTranslator() {
+			
+			@Override
+			public InetSocketAddress translate(InetSocketAddress address) {
+				String newIp = translateIP(address.getAddress()
+						.getHostAddress(), template);
+				return new InetSocketAddress(newIp, address.getPort());
+			}
+			
+			@Override
+			public void init(Cluster cluster) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void close() {
+				// TODO Auto-generated method stub
+				
+			}
+		});
         return cluster;
     }
 
@@ -77,5 +102,43 @@ public class CassandraConfig {
         CassandraSessionFactoryBean session = session();
         return new CassandraTemplate(session.getObject(), session.getConverter());
     }
+    
+    public static String translateIP(String orgIp, String template) {
+		String translatedIP = orgIp;
+		if (template!=null && !template.isEmpty()
+				&& orgIp!=null && !orgIp.isEmpty()) {
+			String[] templates = template.split(",");
+			for (String t : templates) {
+				String[] tmp = t.split(":");
+				if (tmp.length == 2) {
+					String templateFrom = tmp[0];
+					String templateTo = tmp[1];
+					String[] orgIpToken = orgIp.split("\\.");
+					String[] templateFromToken = templateFrom.split("\\.");
+					String[] templateToToken = templateTo.split("\\.");
+					StringBuilder newIP = new StringBuilder("");
+					boolean isMatch = true;
+					for (int i = 0; i < orgIpToken.length; i++) {
+						if (templateFromToken[i].equals("*")) {
+							newIP.append(orgIpToken[i]).append(".");
+						} else if (templateFromToken[i].equals(orgIpToken[i])) {
+							newIP.append(templateToToken[i]).append(".");
+						} else {
+							isMatch = false;
+							break;
+						}
+					}
+					if (isMatch) {
+						translatedIP = newIP.toString();
+						translatedIP = translatedIP.substring(0,
+								translatedIP.length() - 1);
+						break;
+					}
+
+				}
+			}
+		}
+		return translatedIP;
+	}
 }
 
